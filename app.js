19c7d40c193a67ead1efac9a0873c255a3663843
mateusgomes01 <mgm6@cin.ufpe.br>
2020-06-03 18:02:26 -0300

    more data & functions + this to access data inside functions

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Shaun'
+        name: 'Shaun',
+        job: 'ninja'
+    },
+    methods: {
+        greet: function(time){
+            // time = this.name; // accessing Data inside the function
+            return 'Good ' + time + ', ' + this.name + '!';
+        }
     }
 });

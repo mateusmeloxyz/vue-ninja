1a6c818d6246d3c38061d9f8de2c764617397480
mateusgomes01 <mgm6@cin.ufpe.br>
2020-06-04 18:22:05 -0300

    Dynamic CSS

--- app.js
+++ app.js
@@ -1,45 +1,18 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: '',
-        age: 20,
-        x: 0,
-        y: 0,
-        a: 0,
-        b: 0
+        available: false,
+        nearby: false
     },
-    methods: {/*
-        add: function(val){
-            this.age+=val;
-        },
-        updateXY: function(event){
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        alert: function(){
-            alert('You clicked me!');
-        },
-        logName: function(){
-            console.log("You entered your name");
-        },
-        logAge: function(){
-            console.log("You entered your age");
-        },
-        add: function(val){
-            console.log('executed for ' + val);
-            return this.age + val;
-        },*/
+    methods: {
+
     },
-    // when used computed the function will only run
-    // if it's dependencies change
     computed: {
-        addToA: function(){
-            console.log('executed for A' );
-            return this.age + this.a;
-        },
-        addToB: function(){
-            console.log('executed for B' );
-            return this.age + this.b;
+        compClasses: function(){
+            return {
+                available: this.available,
+                nearby: this.nearby
+            }
         }
     }
 });

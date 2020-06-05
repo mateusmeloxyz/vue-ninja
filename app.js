344e8fd1c39ff5d799694163a3f97a8a73e4487a
mateusgomes01 <mgm6@cin.ufpe.br>
2020-06-04 22:08:56 -0300

    components to create reusable code

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
-var one = new Vue({
-    el: '#vue-app-one',
-    data: {
-        title: 'Vue App One'
+Vue.component('greeting', {
+    template: '<p>Hey there, I am a {{name}}. <button v-on:click="changeName">Change name</button></p>',
+    data: function(){ // components require a function to data so that the data is diferent in each vue instance
+        return {
+            name: 'Yoshi'
+        }
     },
     methods: {
-
-    },
-    computed: {
-        greet: function(){
-            return 'Hello from app one :)'
+        changeName: function(){
+            this.name = "Mario"
         }
     }
 });
 
-var two = new Vue({
-    el: '#vue-app-two',
-    data: {
-        title: 'Vue App Two'
-    },
-    methods: {
-        changeTitle: function(){
-            one.title = "Title changed";
-        }
-    },
-    computed: {
-        greet: function(){
-            return 'Yo dudes, this is app 2 speaking to ya :)'
-        }        
-    }
+new Vue({
+    el: '#vue-app-one'
 });
 
-two.title = "Changed from outside";
+new Vue({
+    el: '#vue-app-two'
+})

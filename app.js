46eb022c90b1a06ad6ffce6502f390c33c1d9dfb
mateusgomes01 <mgm6@cin.ufpe.br>
2020-06-04 22:24:25 -0300

    refs

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-Vue.component('greeting', {
-    template: '<p>Hey there, I am a {{name}}. <button v-on:click="changeName">Change name</button></p>',
-    data: function(){ // components require a function to data so that the data is diferent in each vue instance
-        return {
-            name: 'Yoshi'
-        }
+new Vue({
+    el: '#vue-app',
+    data: {
+        output: 'Your fav food'
     },
     methods: {
-        changeName: function(){
-            this.name = "Mario"
+        readRefs: function(){
+            console.log(this.$refs.test.innerText);
+            this.output = this.$refs.input.value;
         }
     }
 });
-
-new Vue({
-    el: '#vue-app-one'
-});
-
-new Vue({
-    el: '#vue-app-two'
-})
